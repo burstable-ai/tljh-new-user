0dd2e9d694301ccf82b220175bf7eb449db685f3
danx0r <dan.miller@eye0.com>
2021-10-19 16:01:13 -0700

    cb_new_user seems quasi-legit

--- cb_new_user.py
+++ cb_new_user.py
@@ -3,5 +3,13 @@ import os
 
 @hookimpl
 def tljh_new_user_create(username):
-    print ("CB_NEW_USER")
-    os.system("touch foofoo")
+    f = open("/etc/passwd")
+    for row in f.readlines():
+        if username in row:
+            pid, path = row.strip().split("::")
+            path = path.split("/")[1]
+            break
+    home = f"/{path}/{username}"
+    os.system(f"echo NEW USER: {username} HOME: {home} >> cb_new_user.log")
+    os.chdir(home)
+    os.system("touch foobaz")
