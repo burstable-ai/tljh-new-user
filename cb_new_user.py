cd73231d7c6a16fa91b06a26dfc47fdd4057fa0b
danx0r <dan.miller@eye0.com>
2021-10-21 15:53:24 -0700

    name change take 2

--- cb_new_user.py
+++ cb_new_user.py
@@ -1,8 +1,33 @@
 from tljh.hooks import hookimpl
 import os
+import sys
+
+class Loggit:
+    def __init__(self, path, prefix):
+        self.path = path
+        self.prefix = prefix
+        try:
+            f = open(self.path, 'a+')
+            f.write(f"~~~{self.prefix}~~~" + "\n")
+            f.close()
+        except:
+            print ("Loggit failed, disabling")
+            self.path = None
+
+    def log(self, *args):
+        s = f"{self.prefix}|" + " ".join([f"{a}" for a in args])
+        if self.path:
+            f = open(self.path, 'a+')
+            f.write(s + "\n")
+            f.close()
+        print (s)
+        sys.stdout.flush()
+
+logg = Loggit("/opt/tljh/state/debug.log", "tnuc2").log
 
 @hookimpl
 def tljh_new_user_create(username):
+    logg(f"tljh_new_user_create({username})")
     f = open("/etc/passwd")
     for row in f.readlines():
         if username in row:
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="cb_new_user",
-    entry_points={"tljh": ["new_user = cb_new_user"]},
+    name="tljh-new-user-data",
+    entry_points={"tljh": ["new-user-data = cb_new_user"]},
     py_modules=["cb_new_user"],
 )
