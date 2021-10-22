baea552b458a08f624a004e4d4d3a14fa3f404c3
danx0r <dan.miller@eye0.com>
2021-10-22 11:32:30 -0700

    simplidy logging

--- tljh_new_user_data.py
+++ tljh_new_user_data.py
@@ -2,32 +2,8 @@ from tljh.hooks import hookimpl
 import os
 import sys
 
-class Loggit:
-    def __init__(self, path, prefix):
-        self.path = path
-        self.prefix = prefix
-        try:
-            f = open(self.path, 'a+')
-            f.write(f"~~~{self.prefix}~~~" + "\n")
-            f.close()
-        except:
-            print ("Loggit failed, disabling")
-            self.path = None
-
-    def log(self, *args):
-        s = f"{self.prefix}|" + " ".join([f"{a}" for a in args])
-        if self.path:
-            f = open(self.path, 'a+')
-            f.write(s + "\n")
-            f.close()
-        print (s)
-        sys.stdout.flush()
-
-logg = Loggit("/opt/tljh/state/debug.log", "tnuc2").log
-
 @hookimpl
 def tljh_new_user_create(username):
-    logg(f"tljh_new_user_create({username})")
     f = open("/etc/passwd")
     for row in f.readlines():
         if username in row:
@@ -35,6 +11,6 @@ def tljh_new_user_create(username):
             path = path.split("/")[1]
             break
     home = f"/{path}/{username}"
-    os.system(f"echo NEW USER: {username} HOME: {home} >> cb_new_user.log")
+    os.system(f"echo NEW USER: {username} HOME: {home} >> tljh-new-user-data.log")
     os.chdir(home)
     os.system("touch foobaz")
