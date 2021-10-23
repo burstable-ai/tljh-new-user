d2a482a16cc7fbf80fda8cbe4dd3f8e0af30b5d2
danx0r <dan.miller@eye0.com>
2021-10-23 14:12:55 -0700

    new_user_script for much pleasure

--- tljh_new_user.py
+++ tljh_new_user.py
@@ -11,6 +11,7 @@ def tljh_new_user_create(username):
             break
     home = f"/{path}/{username}"
     os.system(f"echo NEW USER: {username} HOME: {home} >> tljh-new-user.log")
+
     if os.path.isdir('new_user_data'):
         os.system(f"echo ' ' copying new_user_data >> tljh-new-user.log")
         err = None
@@ -20,7 +21,17 @@ def tljh_new_user_create(username):
             f=open("tljh-new-user.log", 'a')
             print (traceback.format_exc(), file=f)
             f.close()
-            err = traceback.format_exception_only()
     else:
         os.system(f"echo ' ' no new_user_data directory found, nothing to copy >> tljh-new-user.log")
-    os.system(f"echo ' ' returns: {err} >> tljh-new-user.log")
+
+    if os.path.exists('new_user_script'):
+        os.system(f"echo ' ' executing new_user_script >> tljh-new-user.log")
+        err = None
+        try:
+            os.system('./new_user_script')
+        except:
+            f=open("tljh-new-user.log", 'a')
+            print (traceback.format_exc(), file=f)
+            f.close()
+    else:
+        os.system(f"echo ' ' no new_user_script found >> tljh-new-user.log")
