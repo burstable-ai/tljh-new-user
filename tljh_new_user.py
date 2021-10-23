09e65d1a7c6315b5648e783cf004105ad3c29f38
danx0r <dan.miller@eye0.com>
2021-10-23 13:32:46 -0700

    rename to just new-user

--- tljh_new_user.py
+++ tljh_new_user.py
@@ -10,17 +10,17 @@ def tljh_new_user_create(username):
             path = path.split("/")[1]
             break
     home = f"/{path}/{username}"
-    os.system(f"echo NEW USER: {username} HOME: {home} >> tljh-new-user-data.log")
+    os.system(f"echo NEW USER: {username} HOME: {home} >> tljh-new-user.log")
     if os.path.isdir('new_user_data'):
-        os.system(f"echo ' ' copying new_user_data >> tljh-new-user-data.log")
+        os.system(f"echo ' ' copying new_user_data >> tljh-new-user.log")
         err = None
         try:
             shutil.copytree("new_user_data", home, dirs_exist_ok=True)
         except:
-            f=open("tljh-new-user-data.log", 'a')
+            f=open("tljh-new-user.log", 'a')
             print (traceback.format_exc(), file=f)
             f.close()
             err = traceback.format_exception_only()
     else:
-        os.system(f"echo ' ' no new_user_data directory found, nothing to copy >> tljh-new-user-data.log")
-    os.system(f"echo ' ' returns: {err} >> tljh-new-user-data.log")
+        os.system(f"echo ' ' no new_user_data directory found, nothing to copy >> tljh-new-user.log")
+    os.system(f"echo ' ' returns: {err} >> tljh-new-user.log")
