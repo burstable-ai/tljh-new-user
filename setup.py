646ada9bff45c1b2fe995dcd32ddb896a0c8d299
danx0r <dan.miller@eye0.com>
2022-03-20 16:56:37 -0700

    0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tljh-new-user",
-    version = "0.1.2",
+    version = "0.1.3",
     entry_points={"tljh": ["new-user = tljh_new_user"]},
     py_modules=["tljh_new_user"],
 )
